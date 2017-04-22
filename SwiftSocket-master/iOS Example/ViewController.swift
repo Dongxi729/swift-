15a213e03c04d6391bae37b700ba41c26819faed
zdx <18259129536@163.com>
2017-04-22 15:22:35 +0800

    change commit
    初始化

---
+++ SwiftSocket-master/iOS Example/ViewController.swift
@@ -0,0 +1,128 @@
+import UIKit
+import SwiftSocket
+
+class ViewController: UIViewController {
+    
+    var client : TCPClient!
+    
+    var timer : Timer!
+    
+    /// 标记用户存在常量。
+    var sendToUSerMsg : String = "888"
+    
+    lazy var textView: UITextView = {
+        let d : UITextView = UITextView.init(frame: self.view.bounds)
+        d.backgroundColor = UIColor.gray
+        return d
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(textView)
+        
+        DispatchQueue.global(qos: .default).async {
+            self.testServer()
+        }
+    }
+    
+    func echoService(client: TCPClient) {
+        
+        /// 传值
+        self.client = client
+        
+        print("Newclient from:\(client.address)[\(client.port)]")
+        
+        DispatchQueue.main.async {
+            self.textView.text = "Newclient from:\(client.address)[\(client.port)]"
+        }
+        
+        DispatchQueue.main.async {
+            
+            
+            self.timer = Timer.scheduledTimer(timeInterval: 10.0, target: self, selector: #selector(self.sendRandomMsg), userInfo: nil, repeats: true)
+            
+            self.timer.fire()
+            
+//            RunLoop.main.add(self.timer, forMode: .commonModes)
+            RunLoop.main.add(self.timer, forMode: RunLoopMode.defaultRunLoopMode)
+        }
+
+        while true {
+            
+            let d = client.read(1024*10)
+           
+            if d == nil {
+                print("\((#file as NSString).lastPathComponent):(\(#line))\n","断开连接")
+                self.timer.invalidate()
+                
+            
+                /// 断开连接后，重置记录常量
+                self.sendToUSerMsg = "888"
+                
+                return
+            } else {
+                let geStr = String(bytes: d!, encoding: String.Encoding.utf8)
+                
+                print("\((#file as NSString).lastPathComponent):(\(#line))\n",geStr as Any)
+                
+                client.send(data: d!)
+            }
+        }
+    }
+    
+    
+    /// 发送消息
+    func sendRandomMsg() -> Void {
+
+        if !self.sendToUSerMsg.contains("用户") {
+            
+            let temp = Int(arc4random_uniform(1000))+1
+            
+            let sendStr : String = "用户" + String(temp) + "在线"
+            
+            self.sendToUSerMsg = sendStr
+        }
+
+
+        let ndata = self.sendToUSerMsg.data(using: .utf8)
+        
+        var int : Int = (ndata?.count)!
+        
+        let data2 : NSMutableData = NSMutableData()
+        
+        data2.append(&int, length: 4)
+        
+        data2.append(ndata!)
+        
+        guard let socket = client else {
+            return
+        }
+        
+        socket.send(data: data2 as Data)
+     
+        
+        print("\((#file as NSString).lastPathComponent):(\(#line))\n")
+    }
+    
+    func testServer() {
+        //        let server = TCPServer(address: "192.168.3.4", port: 8411)
+        let server = TCPServer(address: "172.30.33.32", port: 8888)
+        
+        
+        switch server.listen() {
+        case .success:
+            while true {
+                if let client = server.accept() {
+                    echoService(client: client)
+                } else {
+                    print("accept error")
+                }
+            }
+        case .failure(let error):
+            
+            
+            print("\((#file as NSString).lastPathComponent):(\(#line))\n",error)
+            print("\((#file as NSString).lastPathComponent):(\(#line))\n",error.localizedDescription)
+        }
+    }
+}
