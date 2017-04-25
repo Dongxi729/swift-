6cc884d3f29499136b57861d4efe79e720d06970
zdx <18259129536@163.com>
2017-04-25 14:29:16 +0800

    图片成功接收。。。

--- SwiftSocket-master/iOS Example/ViewController.swift
+++ SwiftSocket-master/iOS Example/ViewController.swift
@@ -5,7 +5,7 @@ class ViewController: UIViewController {
     
     var client : TCPClient!
     
-    var timer : Timer!
+    private weak var timer : Timer!
     
     /// 标记用户存在常量。
     var sendToUSerMsg : String = "888"
@@ -38,32 +38,32 @@ class ViewController: UIViewController {
         
         DispatchQueue.main.async {
             
-            
             self.timer = Timer.scheduledTimer(timeInterval: 10.0, target: self, selector: #selector(self.sendRandomMsg), userInfo: nil, repeats: true)
             
             self.timer.fire()
-            
-//            RunLoop.main.add(self.timer, forMode: .commonModes)
+
             RunLoop.main.add(self.timer, forMode: RunLoopMode.defaultRunLoopMode)
         }
-
+        
         while true {
             
-            let d = client.read(1024*10)
-           
+            var d = client.read(1024*10)
+            
             if d == nil {
                 print("\((#file as NSString).lastPathComponent):(\(#line))\n","断开连接")
                 self.timer.invalidate()
                 
-            
+                
                 /// 断开连接后，重置记录常量
                 self.sendToUSerMsg = "888"
                 
                 return
             } else {
-                let geStr = String(bytes: d!, encoding: String.Encoding.utf8)
+                print(d!)
+                //d?[10] = (d?[10])!+3
+                //let geStr = String(bytes: d!, encoding: String.Encoding.utf8)
                 
-                print("\((#file as NSString).lastPathComponent):(\(#line))\n",geStr as Any)
+                //print("\((#file as NSString).lastPathComponent):(\(#line))\n",geStr as Any)
                 
                 client.send(data: d!)
             }
@@ -73,7 +73,8 @@ class ViewController: UIViewController {
     
     /// 发送消息
     func sendRandomMsg() -> Void {
-
+        
+        /// 生成随机用户名称,并发送
         if !self.sendToUSerMsg.contains("用户") {
             
             let temp = Int(arc4random_uniform(1000))+1
@@ -82,32 +83,33 @@ class ViewController: UIViewController {
             
             self.sendToUSerMsg = sendStr
         }
-
-
+        
+        
         let ndata = self.sendToUSerMsg.data(using: .utf8)
         
         var int : Int = (ndata?.count)!
         
         let data2 : NSMutableData = NSMutableData()
         
+        /// 添加包头，反之粘包
         data2.append(&int, length: 4)
         
+        /// 添加要发送的内容
         data2.append(ndata!)
         
         guard let socket = client else {
             return
         }
         
-        socket.send(data: data2 as Data)
-     
+        
+       // socket.send(data: data2 as Data)
+        
         
         print("\((#file as NSString).lastPathComponent):(\(#line))\n")
     }
     
     func testServer() {
-        //        let server = TCPServer(address: "192.168.3.4", port: 8411)
-        let server = TCPServer(address: "172.30.33.32", port: 8888)
-        
+        let server = TCPServer(address: "192.168.3.4", port: 8411)
         
         switch server.listen() {
         case .success:
@@ -116,6 +118,9 @@ class ViewController: UIViewController {
                     echoService(client: client)
                 } else {
                     print("accept error")
+                    
+                    return
+                    
                 }
             }
         case .failure(let error):
